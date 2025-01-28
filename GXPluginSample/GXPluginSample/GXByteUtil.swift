c31575222486182b965728088b7f19a2a689ec8c
Gin <279694479@qq.com>
2025-01-28 19:34:02 +0800

    1

--- GXPluginSample/GXPluginSample/GXByteUtil.swift
+++ GXPluginSample/GXPluginSample/GXByteUtil.swift
@@ -10,11 +10,11 @@ import UIKit
 // MARK: - 字节
 // MARK: swift里，Byte就是UInt8，一个字节，8位
 public extension UInt8 {
-    var hexString: String {
+    var gx_hexString: String {
         let str = String(format:"0x%02x", self)
         return str
     }
-    var bString: String {
+    var gx_bString: String {
         // 转2进制，输出的内容是有效位，左侧的0没有
         var str = String(self, radix: 2)
         // 一个UInt用二进制来方，是8位
@@ -25,8 +25,8 @@ public extension UInt8 {
         }
         return str
     }
-    // 读取对应位置上的数字（结果是0或者1）
-    func readBit(index: Int) -> UInt8 {
+    /// 读取对应位置上的数字（结果是0或者1）
+    func gx_readBit(index: Int) -> UInt8 {
         if index < 0 || index > 7 {
             return self
         }
@@ -41,8 +41,8 @@ public extension UInt8 {
         let result = val >> moveStep
         return result
     }
-    // 给对应位置上设置指定的值（只能设置0或者1），返回修改后的新值
-    func writeBit(index: Int, value: Int) -> UInt8 {
+    /// 给对应位置上设置指定的值（只能设置0或者1），返回修改后的新值
+    func gx_writeBit(index: Int, value: Int) -> UInt8 {
         // 一个字节只有8位
         if index < 0 || index > 7 {
             return self
@@ -66,12 +66,12 @@ public extension UInt8 {
         let result = otherVal | setVal
         return result
     }
-    // 按bit位得到数字
-    func numberVal(range: NSRange) -> Int {
+    /// 按bit位得到数字
+    func gx_numberVal(range: NSRange) -> Int {
         var binaryString: String = ""
         let start = range.location, end = range.location + range.length
         for i in start..<end {
-            let bit: UInt8 = self.readBit(index: i)
+            let bit: UInt8 = self.gx_readBit(index: i)
             binaryString += "\(bit)"
         }
         guard let decimalNumber = Int(binaryString, radix: 2) else {
@@ -84,20 +84,20 @@ public extension UInt8 {
 // MARK: - Data
 // MARK: swift里，Data 与 [UInt8]、NSData是等价的
 public extension Data {
-    var nsData: NSData {
+    var gx_nsData: NSData {
         return (self as NSData)
     }
-    var bytes: [UInt8] {
+    var gx_bytes: [UInt8] {
         return [UInt8](self)
     }
-    func subData(range: NSRange) -> Data {
-        return self.nsData.subdata(with: range)
+    func gx_subData(range: NSRange) -> Data {
+        return self.gx_nsData.subdata(with: range)
     }
-    func subBytes(range: NSRange) -> [UInt8] {
-        return self.subData(range: range).bytes
+    func gx_subBytes(range: NSRange) -> [UInt8] {
+        return self.gx_subData(range: range).gx_bytes
     }
     // MARK: - Data\Bytes -> 数字
-    func numberVal<T: FixedWidthInteger>() -> T {
+    func gx_numberVal<T: FixedWidthInteger>() -> T {
         // 当前类型需要几个字节来放
         let size = MemoryLayout<T>.size
         // 如果只需要一个字节，说明就是UInt8
@@ -106,54 +106,54 @@ public extension Data {
         let addCount = size - self.count
         if addCount > 0 {
             // 说明长度不够，需要在前面补0
-            let addData = ([UInt8](repeating: 0, count: addCount)).data
+            let addData = ([UInt8](repeating: 0, count: addCount)).gx_data
             valData = addData + valData
         } else if addCount < 0 {
             // 长度超过所需的长度，只使用后面的内容
-            valData = self.subData(range: NSRange(location: self.count-size, length: size))
+            valData = self.gx_subData(range: NSRange(location: self.count-size, length: size))
         }
-        valData.nsData.getBytes(&val, length: size)
+        valData.gx_nsData.getBytes(&val, length: size)
         let result = T(bigEndian: val)
         return result
     }
-    var strVal: String? {
+    var gx_strVal: String? {
         return String(data: self, encoding: .utf8)
     }
-    var hexString: String {
-        self.map({ $0.hexString }).joined(separator: " ")
+    var gx_hexString: String {
+        self.map({ $0.gx_hexString }).joined(separator: " ")
     }
-    var bString: String {
-        self.map({ $0.bString }).joined(separator: " ")
+    var gx_bString: String {
+        self.map({ $0.gx_bString }).joined(separator: " ")
     }
 }
 
 // MARK: - 字节数组
 public extension Array where Element == UInt8 {
-    var data: Data {
+    var gx_data: Data {
         return Data(self)
     }
-    var nsData: NSData {
-        return self.data.nsData
+    var gx_nsData: NSData {
+        return self.gx_data.gx_nsData
     }
-    func subData(range: NSRange) -> Data {
-        return self.data.subData(range: range)
+    func gx_subData(range: NSRange) -> Data {
+        return self.gx_data.gx_subData(range: range)
     }
-    func subBytes(range: NSRange) -> [UInt8] {
-        return self.data.subBytes(range: range)
+    func gx_subBytes(range: NSRange) -> [UInt8] {
+        return self.gx_data.gx_subBytes(range: range)
     }
-    func numberVal<T: FixedWidthInteger>() -> T {
-        return self.data.numberVal()
+    func gx_numberVal<T: FixedWidthInteger>() -> T {
+        return self.gx_data.gx_numberVal()
     }
-    var strVal: String? {
-        return self.data.strVal
+    var gx_strVal: String? {
+        return self.gx_data.gx_strVal
     }
-    var hexString: String {
-        return self.data.hexString
+    var gx_hexString: String {
+        return self.gx_data.gx_hexString
     }
-    var bString: String {
-        return self.data.bString
+    var gx_bString: String {
+        return self.gx_data.gx_bString
     }
-    func crc16() -> UInt16 {
+    func gx_crc16() -> UInt16 {
         let count = self.count
         var crc: UInt16 = 0
         for i in 0..<count {
@@ -172,7 +172,7 @@ public extension Array where Element == UInt8 {
 
 // MARK: - 数字转化 -> Data\Bytes
 public extension FixedWidthInteger {
-    var bytes: [UInt8] {
+    var gx_bytes: [UInt8] {
         // 当前类型需要几个字节来放
         let size = MemoryLayout<Self>.size
         var bytes = [UInt8]()
@@ -184,17 +184,17 @@ public extension FixedWidthInteger {
         }
         return bytes
     }
-    var data: Data {
-        return self.bytes.data
+    var gx_data: Data {
+        return self.gx_bytes.gx_data
     }
 }
 
 // MARK: - 范围扩展
 public extension NSRange {
-    var nextLocation: Int {
+    var gx_nextLocation: Int {
         return self.location + self.length
     }
-    func nextRange(length: Int) -> NSRange {
-        return NSRange(location: self.nextLocation, length: length)
+    func gx_nextRange(length: Int) -> NSRange {
+        return NSRange(location: self.gx_nextLocation, length: length)
     }
 }
